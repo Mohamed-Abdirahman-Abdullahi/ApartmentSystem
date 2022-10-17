b2aa6f2a6655d2f5570b2285aa79cc23fe341bb2
Mohamed <nadaaramn@gmail.com>
2022-10-17 11:19:16 +0300

    adding expense route

--- backend/main.js
+++ backend/main.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const error = require("./middleware/error");
 
-const users = require('./routes/users/users')
-const employees = require('./routes/employees/employees');
-const tenants = require('./routes/tenants/tenants')
-const apartments = require('./routes/apartments/apartments');
-const floors = require('./routes/floors/floors');
-const units = require('./routes/units/units');
-const departments = require('./routes/departments/departments');
-const visitors = require('./routes/visitors/visitors');
-const guarantors = require('./routes/guarantors/guarantors');
-const inboxes = require('./routes/inboxes/inboxes');
-const income = require('./routes/income/income');
+const users = require("./routes/users/users");
+const employees = require("./routes/employees/employees");
+const tenants = require("./routes/tenants/tenants");
+const apartments = require("./routes/apartments/apartments");
+const floors = require("./routes/floors/floors");
+const units = require("./routes/units/units");
+const departments = require("./routes/departments/departments");
+const visitors = require("./routes/visitors/visitors");
+const guarantors = require("./routes/guarantors/guarantors");
+const inboxes = require("./routes/inboxes/inboxes");
+const income = require("./routes/income/income");
+const expenses = require("./routes/expenses/expenses");
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/users', users);
-app.use('/api/employees', employees);
-app.use('/api/tenants', tenants);
-app.use('/api/apartments', apartments);
-app.use('/api/floors', floors);
-app.use('/api/units', units);
-app.use('/api/departments', departments);
-app.use('/api/visitors', visitors);
-app.use('/api/guarantors', guarantors);
-app.use('/api/inboxes', inboxes);
-app.use('/api/income', income);
+app.use("/api/users", users);
+app.use("/api/employees", employees);
+app.use("/api/tenants", tenants);
+app.use("/api/apartments", apartments);
+app.use("/api/floors", floors);
+app.use("/api/units", units);
+app.use("/api/departments", departments);
+app.use("/api/visitors", visitors);
+app.use("/api/guarantors", guarantors);
+app.use("/api/inboxes", inboxes);
+app.use("/api/income", income);
+app.use("/api/expenses", expenses);
 
-app.get('/', (req, res)=>{
-    res.send("Server set up successfull.")
-})
+app.use(error);
 
-app.listen('9000', ()=>{
-    console.log("Server running at port {9000} ")
-})
+app.get("/", (req, res) => {
+  res.send("Server set up successfull.");
+});
+
+app.listen("9000", () => {
+  console.log("Server running at port {9000} ");
+});
---
+++ backend/models/expenses/expense.js
@@ -0,0 +1,71 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    account: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 255,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 255,
+    },
+    type: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 255,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 255,
+    },
+    memo: {
+      type: String,
+      min: 2,
+      max: 255,
+    },
+    receiptNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    amount: {
+      type: mongoose.Schema.Types.Decimal128,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+const validate = (expense) => {
+  const schema = Joi.object({
+    account: Joi.string().min(2).max(255).required(),
+    number: Joi.string().required(),
+    description: Joi.string().min(2).max(255).required(),
+    type: Joi.string().min(2).max(255).required(),
+    category: Joi.string().min(2).max(255).required(),
+    memo: Joi.string().min(2).max(255),
+    receiptNo: Joi.string().required(),
+    amount: Joi.string().required(),
+  });
+};
+
+module.exports = {
+  Expense,
+  validate,
+};
